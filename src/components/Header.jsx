6e4b6e5f1bce056d3d680e4ec589d5b89a270ae5
Puja <pujak816@gmail.com>
2023-10-02 10:49:04 +0530

    style: css added to components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import hamburger from "../assets/hamburger.png";
 
 const Header = () => {
   return (
-    <header className="bg-gray-100 flex justify-between items-center px-10 py-2  w-full shadow-xl">
+    <header className="bg-white flex justify-between items-center px-10 py-2 mt-4  w-[275px] mx-auto shadow-xl">
       <div className="w-10">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6iEmu_Fhfpy3Cg9nmBSA05f_J17WMd1xMj2ODv9gGpg&s"
